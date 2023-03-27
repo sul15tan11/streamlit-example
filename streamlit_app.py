dfbaf7e558486104a14ffd81c38ab8151cdac321
sul15tan11 <psxsa16@nottingham.ac.uk>
2023-03-27 14:31:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,7 @@ def generate_response(prompt):
     completions = openai.Completion.create (
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
+       
     )
 
     message = completions.choices[0].text
