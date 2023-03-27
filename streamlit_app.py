96fc983628ad8cee53dbee30783c35932db9b0b1
sul15tan11 <psxsa16@nottingham.ac.uk>
2023-03-27 15:00:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ article_text = st.text_area("Enter your scientific texts to summarize")
 
 # Create Radio Buttons
 output_size = st.radio( label = "What kind of output do you want?", 
-                        options= [“To-The-Point”, “Concise”, “Detailed”]
+                        options= ["To-The-Point", "Concise", "Detailed"]
                      )
 
 
